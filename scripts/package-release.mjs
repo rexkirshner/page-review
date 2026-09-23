aee6d4663902f420ffd6083f886d817da41dbd01
Rex Kirshner <github@rexkirshner.com>
2026-09-22 19:50:54 -0700

    Make release archives reproducible

--- scripts/package-release.mjs
+++ scripts/package-release.mjs
@@ -37,5 +37,6 @@ for (const required of ["background.js", "content.js", "icons/icon128.png"]) {
 
 await mkdir("release", { recursive: true });
 const filename = `feedback-packet-${manifest.version}.zip`;
-await writeFile(path.join("release", filename), zipSync(Object.fromEntries(files), { level: 9 }));
+const releaseTimestamp = new Date(1980, 0, 1);
+await writeFile(path.join("release", filename), zipSync(Object.fromEntries(files), { level: 9, mtime: releaseTimestamp }));
 console.log(`Created release/${filename} with ${files.length} files.`);
